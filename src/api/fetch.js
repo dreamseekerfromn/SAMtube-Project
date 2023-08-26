0639dbbfd02dc438f548c55b9394df70b294765a
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-08-25 20:53:12 -0400

    changed default quary value in fetch.js

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_BASE_API_KEY;
 const API_URL = `https://www.googleapis.com/youtube/v3`;
 
-export default async function testAPI(maxResults = 8, query="soccer"){
+export default async function testAPI(maxResults = 8, query=""){
     console.log(API_KEY)
     if(query){
         return await fetch(`${API_URL}/search?part=snippet&key=${API_KEY}&maxResults=${maxResults}&q=${query}`);
