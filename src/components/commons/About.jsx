50a248b57e8270a2acda26db13a50297a88b052f
Addis Jackson <addisjackson@pursuit.org>
2023-09-11 19:03:28 -0400

    completed About

--- src/components/commons/About.jsx
+++ src/components/commons/About.jsx
@@ -8,7 +8,43 @@
 export default function About(){
     return(
         <div>
-            <p>About page</p>
+            <h2>About page</h2>
+            <p>
+                
+The SAMtube project was an arduous one to say the least. To clone an app created by the minds at google, with hundreds of developers working at it, innumerable updates to finesse all its parts and input from the public all in an effort to create a most user-friendly, easily accessible, world-accepted app should have stopped us dead in our tracks but we forged on into the depths of coding vowing only to return with something we know people will appreciate, that is, SAMtube. Its quick reaction to user inputs and api queries is bar none, except for youtube, and its potential for growth even succeeds youtube itself.
+
+Our group consisted of Sung Yi, coder extraordinaire, Mikal wazeerud-din- stylist to star-like coders, and Addis Jackson, every man coder. Our task, now before us since August 23rd, 2023, promised to be one that would take us into google’s vast network of apis, where without a unique key that needed to be renewed weekly,  all could be lost. We had only the Gods, Tim McKiernan and his cohort, Gigi Scarborough, as our guide through this treacherous terrain and our knowledge and experience, as was given to us though life.
+
+Sung Yi, an avid coder, with a quiet but stern and demanding presence is a recent graduate with a aBachelors degree from University of Missouri - St Louis. He graduated from the department of Computer Sciences with a good base using C program language. He was undaunting in his effort to be a FULL-STACK web developer which led him to Pursuit where he is now a fellow blazing his path toward that goal. He dazzled in the coding for SAMtube and island always ensured at day’s end, a working deployable design of the app was pushed to github, which was a good progressive starting point for those of us who were mistake-proned.  SAMtuibe, now in his past serves as a reminder that all other code challenges should cower at his name and prowess with the keys of a Macbook. They should know that there is no other choice to be made than to concede to his great skill.
+
+Mikal wazeerud-din, a graduate of Boston University, with a Bachelor of Science degree in Cinematography and Film/Video Production, has an eye for design that we all  should be lucky to see in person. His methods, detail, silhouettes and flair all in combination describe him as having, cliche as it may seem,  je ne sais quio. He has too now joined the future leaders in code here at Pursuit, and applying his design to code will no doubt leave us in breathtaking anticipation for his future work. Samtube was no different when applying this style, he so modestly wield,  and he  has taken it to a level that all other groups now  envy.
+Addis Jackson, a former graduate of the University of Massachusetts, with emphases on chemistry and chemical engineering,  now joined pursuit  with a determination to apply coding to the abstract subjects in education. To make visible those things not readily seen and therefore not understood by most students such as the atom, forces and even wave and particle dynamics. SAMtube served as a hurdle that he, relying on his  other group members, worked diligently to subdue this monster of a project and, we are happy to say, succeeded in our task. He helped with some code and the cohesiveness of everyone’s code during merges as well as trying to ensure we all adhere to the architecture we had agreed on in the beginning.
+
+Our group norms were simple:
+
+GROUP NORMS: Enabling our best to shine through
+We recognize the value of our diverse experiences, styles, backgrounds and perspectives; we encourage one another to share our points of view and listen respectfully. Everyone has a voice even when one decision needs to be made.
+
+
+We are present and actively contribute by refraining from iPhone/cell phone use during meetings and in public spaces unless it is an urgent matter or directly related to the meeting.
+We hold ourselves and each other accountable to meet commitments; we accept personal responsibility for decisions and actions. We provide timely updates when delays or obstacles arise.
+
+
+
+
+During the break our intent is to meet on:
+
+Slack is the medium of choice for quick memos, questions and inserts but whenever we can work together at the same time, zoom, trello and github are the resources we will use.
+
+Github Repository:  SAMtube Github Repo
+
+
+.
+
+
+
+
+</p></>
             <p>Sung Yi <a href="https://github.com/dreamseekerfromn/"><img src="src/assets/GitHub-Mark.png" style={{width:"80px",height:"80px"}}></img></a></p>
             <p>Mikal Wazeerud-din <a href="#"><img src="src/assets/GitHub-Mark.png" style={{width:"80px",height:"80px"}}></img></a></p>
             <p>Addis Jackson <a href="https://github.com/addisjackson"><img src="src/assets/GitHub-Mark.png" style={{width:"80px",height:"80px"}}></img></a></p>
